499707f9aab1caa183be16d9aa438987f5264796
Sharanya-A2005 <sharanyaa078@gmail.com>
2025-07-23 08:50:01 +0530

    Create emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+def emotion_detector(text_to_analyze):
+    if not text_to_analyze.strip():  # handles blank input
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }, 400
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = max(emotions, key=emotions.get)
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    },200
